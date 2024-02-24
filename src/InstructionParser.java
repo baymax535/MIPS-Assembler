4a670dcbf8cf1b12e981ac7d3c6b3ae1097d81d4
JHuang95 <huang95@uwm.edu>
2024-02-24 17:44:30 -0600

    basic methods for InstructionParset

--- src/InstructionParser.java
+++ src/InstructionParser.java
@@ -1,3 +1,28 @@
+//Takes single line of MIPS assembly code as input, breaks down into recognizable parts (operation, registers, immediate vlaue, offset/label)
 public class InstructionParser {
+    //Constructor
+    public InstructionParser() {
+        //TODO
+    }
+
+    //Main parsing method
+    public ParsedInstruction parse(String instruction){
+        //TODO
+    }
+
+    //Helper
+    public String removeComments(String instruction){
+        //TODO
+    }
+
+    public String[] splitComponenets(String instruction){
+        //TODO
+    }
+
+    private boolean validateComponents(String[] components){
+        //TODO
+    }
+
+    //maybe more methods if needed
 
 }
