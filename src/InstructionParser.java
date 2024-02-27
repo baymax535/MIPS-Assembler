d8a8e282d8f2920b65059e3fba4dac83a89683a6
JHuang95 <huang95@uwm.edu>
2024-02-26 22:42:21 -0600

    InstructionParser formatting changes

--- src/InstructionParser.java
+++ src/InstructionParser.java
@@ -1,41 +1,42 @@
 //Takes single line of MIPS assembly code as input, breaks down into recognizable parts (operation, registers, immediate vlaue, offset/label)
 public class InstructionParser {
     /**
-    * Constructs an instance of the InstructionParser.
-     * This constructor initializes any necessary state or configurations required for parsing.
-    */
+     * Constructs an instance of the InstructionParser.
+     * This constructor initializes any necessary state or configurations required
+     * for parsing.
+     */
     public InstructionParser() {
-        //TODO
+        // TODO
     }
-    
+
     public class ParsedInstruction {
-    	private String mnemonic;
-    	private String targetRegister;
-    	private String sourceRegister1;
-    	private String sourceRegister2;
-    	
-    	ParsedInstruction(String mnemonic, String targetRegister, String sourceRegister1, String sourceRegister2) {
-    		this.mnemonic = mnemonic;
-    		this.targetRegister = targetRegister;
-    		this.sourceRegister1 = sourceRegister1;
-    		this.sourceRegister2 = sourceRegister2;
-    	}
-    	
-    	public String getMnemonic() {
-    		return mnemonic;
-    	}
-    	
-    	public String getTarget() {
-    		return targetRegister;
-    	}
-    	
-    	public String getSourceRegister1() {
-    		return sourceRegister1;
-    	}
-    	
-    	public String getSourceRegister2() {
-    		return sourceRegister2;
-    	}
+        private String mnemonic;
+        private String targetRegister;
+        private String sourceRegister1;
+        private String sourceRegister2;
+
+        ParsedInstruction(String mnemonic, String targetRegister, String sourceRegister1, String sourceRegister2) {
+            this.mnemonic = mnemonic;
+            this.targetRegister = targetRegister;
+            this.sourceRegister1 = sourceRegister1;
+            this.sourceRegister2 = sourceRegister2;
+        }
+
+        public String getMnemonic() {
+            return mnemonic;
+        }
+
+        public String getTarget() {
+            return targetRegister;
+        }
+
+        public String getSourceRegister1() {
+            return sourceRegister1;
+        }
+
+        public String getSourceRegister2() {
+            return sourceRegister2;
+        }
     }
 
     /**
@@ -48,18 +49,19 @@ public class InstructionParser {
      *                                  unsupported.
      */
     public ParsedInstruction parse(String instruction) {
-    	instruction = removeComments(instruction);
-    	String[] arr = splitComponents(instruction);
-    	boolean isValid = validateComponents(arr);
-    	ParsedInstruction ans = null;
-    	if(isValid) {
-    		
-    	} else return ans;
-    	
-    	return ans;
+        instruction = removeComments(instruction);
+        String[] arr = splitComponents(instruction);
+        boolean isValid = validateComponents(arr);
+        ParsedInstruction ans = null;
+        if (isValid) {
+
+        } else
+            return ans;
+
+        return ans;
     }
 
-    //Helper
+    // Helper
     /**
      * Removes comments from a MIPS assembly instruction.
      * Comments are identified by a '#' character and everything following it is
@@ -69,18 +71,18 @@ public class InstructionParser {
      *                    comments.
      * @return The instruction with comments removed.
      */
-    public String removeComments(String instruction){
-        //TODO
-    	String result = instruction;
-    	int index;
-    	for(index = 0;index<result.length();index++) {
-    		if (instruction.charAt(index) == '#') {
+    public String removeComments(String instruction) {
+        // TODO
+        String result = instruction;
+        int index;
+        for (index = 0; index < result.length(); index++) {
+            if (instruction.charAt(index) == '#') {
                 break;
             }
-    	}
-    	result = instruction.substring(0, index);
-    	
-    	return result;
+        }
+        result = instruction.substring(0, index);
+
+        return result;
     }
 
     /**
@@ -91,14 +93,14 @@ public class InstructionParser {
      * @return An array of String where the first element is the mnemonic and the
      *         subsequent elements are the operands.
      */
-    
-    public String[] splitComponents(String instruction){
-        //TODO
-    	String[] parts = instruction.split(" `",3);//Spliting the windows instruction at `
-    	if (parts.length != 3) {
+
+    public String[] splitComponents(String instruction) {
+        // TODO
+        String[] parts = instruction.split(" `", 3);// Spliting the windows instruction at `
+        if (parts.length != 3) {
             throw new IllegalArgumentException("Invalid number of components in the instruction.");
         }
-    	return parts;
+        return parts;
     }
 
     /**
@@ -110,37 +112,38 @@ public class InstructionParser {
      * @return true if the components are valid according to the instruction set
      *         specifications; false otherwise.
      */
-    private boolean validateComponents(String[] components){
-        //TODO
-    	// Validate mnemonic
+    private boolean validateComponents(String[] components) {
+        // TODO
+        // Validate mnemonic
         if (!isValidMnemonic(components[0])) {
             return false;
         }
         // Validate operands
-        
-        return true;	
+
+        return true;
     }
 
     // Helper method to extract operands from the instruction component
     private String[] extractOperands(String[] components) {
         // TODO
-    	String[] operands = new String[2];
-    	if(components[2].contains(", ")) {
-    		operands = components[2].split(", ", 2);
-    	}
-    	return operands;
+        String[] operands = new String[2];
+        if (components[2].contains(", ")) {
+            operands = components[2].split(", ", 2);
+        }
+        return operands;
     }
 
-    //maybe more methods if needed
-    
+    // maybe more methods if needed
+
     private boolean isValidMnemonic(String mnemonic) {
         // List of valid mnemonics
         String[] validMnemonics = {
-            "add", "addiu", "and", "andi", "beq", "bne", "j", "lui", "lw", "or", "ori", "slt", "sub", "sw", "syscall"
+                "add", "addiu", "and", "andi", "beq", "bne", "j", "lui", "lw", "or", "ori", "slt", "sub", "sw",
+                "syscall"
         };
 
         // Check if the mnemonic is in the list of valid mnemonics
-        for (int i = 0; i< validMnemonics.length;i++) {
+        for (int i = 0; i < validMnemonics.length; i++) {
             if (validMnemonics[i].equals(mnemonic)) {
                 return true;
             }
@@ -149,75 +152,76 @@ public class InstructionParser {
     }
 
     private boolean isValidOperandCount(String mnemonic, int operandCount) {
-    	
-    	if (mnemonic.equals("add")) {
-		    System.out.println("add please");
-		} 
-		
-		else if (mnemonic.equals("addiu")) {
-		    System.out.println("addiu please");
-		} 
-		
-		else if (mnemonic.equals("and")) {
-		    System.out.println("and please");
-		} 
-		
-		else if (mnemonic.equals("andi")) {
-		    System.out.println("andi please");
-		} 
-		
-		else if (mnemonic.equals("beq")) {
-		    System.out.println("beq please");
-		} 
-		
-		else if (mnemonic.equals("bne")) {
-		    System.out.println("bne please");
-		} 
-		
-		else if (mnemonic.equals("j")) {
-		    System.out.println("j please");
-		} 
-		
-		else if (mnemonic.equals("lui")) {
-		    System.out.println("lui please");
-		} 
-		
-		else if (mnemonic.equals("lw")) {
-		    System.out.println("lw please");
-		} 
-		
-		else if (mnemonic.equals("or")) {
-		    System.out.println("or please");
-		} 
-		
-		else if (mnemonic.equals("ori")) {
-		    System.out.println("ori please");
-		} 
-		
-		else if (mnemonic.equals("slt")) {
-		    System.out.println("slt please");
-		} 
-		
-		else if (mnemonic.equals("sub")) {
-		    System.out.println("sub please");
-		} 
-		
-		else if (mnemonic.equals("sw")) {
-		    System.out.println("sw please");
-		} 
-		
-		else if (mnemonic.equals("syscall")) {
-		    System.out.println("syscall please");
-		} 
-		
-		else return false;
-    	return true;
+
+        if (mnemonic.equals("add")) {
+            System.out.println("add please");
+        }
+
+        else if (mnemonic.equals("addiu")) {
+            System.out.println("addiu please");
+        }
+
+        else if (mnemonic.equals("and")) {
+            System.out.println("and please");
+        }
+
+        else if (mnemonic.equals("andi")) {
+            System.out.println("andi please");
+        }
+
+        else if (mnemonic.equals("beq")) {
+            System.out.println("beq please");
+        }
+
+        else if (mnemonic.equals("bne")) {
+            System.out.println("bne please");
+        }
+
+        else if (mnemonic.equals("j")) {
+            System.out.println("j please");
+        }
+
+        else if (mnemonic.equals("lui")) {
+            System.out.println("lui please");
+        }
+
+        else if (mnemonic.equals("lw")) {
+            System.out.println("lw please");
+        }
+
+        else if (mnemonic.equals("or")) {
+            System.out.println("or please");
+        }
+
+        else if (mnemonic.equals("ori")) {
+            System.out.println("ori please");
+        }
+
+        else if (mnemonic.equals("slt")) {
+            System.out.println("slt please");
+        }
+
+        else if (mnemonic.equals("sub")) {
+            System.out.println("sub please");
+        }
+
+        else if (mnemonic.equals("sw")) {
+            System.out.println("sw please");
+        }
+
+        else if (mnemonic.equals("syscall")) {
+            System.out.println("syscall please");
+        }
+
+        else
+            return false;
+        return true;
     }
 
     private boolean isValidOperand(String[] operand) {
         // Add logic to check if the operand format is valid
         // Return true if valid, false otherwise
-    	//TODO
+        // TODO
         return false;
     }
 }
