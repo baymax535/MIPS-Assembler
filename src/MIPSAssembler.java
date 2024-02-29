67c3fa6c834bad30a32ace6ea14d08178b5ae834
JHuang95 <huang95@uwm.edu>
2024-02-28 21:23:20 -0600

    Revert "Merge branch 'main' of https://github.com/baymax535/MIPS-Assembler"
    This reverts commit 156c0dc955a626285c9912a995f19fb2e4a6745f, reversing
    changes made to e768ddee0d222d654a8e442930bf91f11209c1a1.

--- src/MIPSAssembler.java
+++ src/MIPSAssembler.java
@@ -1,28 +1,29 @@
-import java.util.Scanner;
-
 public class MIPSAssembler {
 	public static void main(String[] args) {
-		//get input MIPS from command line
-		//create InstructionParser object and parse
-		//Create instructionConverter object and convert
-		//output
-		
-	if(args.length == 0){
-		System.out.println("Please provide a MIPS assembly instruction as an argument");
-		return;
-	}
-	
-	String mipsInstruction = args[0];
+		// get input MIPS from command line
+		// create InstructionParser object and parse
+		// Create instructionConverter object and convert
+		// output
 
-	InstructionParser parser = new InstructionParser();
-	InstructionConverter converter = new InstructionConverter(null);
-	
-	ParsedInstruction parsedInstruction = parser.parse(mipsInstruction);
-	String machineCode = converter.convertToMachineCode(parsedInstruction);
+		if (args.length != 1) {
+			// System.err.println("Usage: java MIPSAssembler \"<instruction>\"");
+			System.exit(1);
+		}
 
-	String formattedMachineCode = Util.formatOutput(machineCode);
+		try {
+			String instruction = args[0];
+			InstructionParser parser = new InstructionParser(instruction);
+			String operation = parser.getOperation();
+			String[] arguments = parser.getArguments();
 
-	System.out.println(formattedMachineCode);
+			String machineCode = InstructionConverter.convertToMachineCode(operation, arguments);
+
+			System.out.println(machineCode);
+		} catch (Exception e) {
+			// System.err.println("Error processing the instruction: " + e.getMessage());
+			// e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 }
